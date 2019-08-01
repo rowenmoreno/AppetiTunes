bc5c5092ba4279da08d3c38e9c8ce8c9efe78f1b
dev-moreno <rowen.m@filipinowebmasters.com>
2019-08-01 15:14:55 +0800

    Initial commit

---
+++ app/src/main/java/com/moreno/searchitunes/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.moreno.searchitunes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+    }
+}
---
+++ app/src/main/java/com/moreno/searchitunes/TrackListActivity.java
@@ -0,0 +1,12 @@
+package com.moreno.searchitunes;
+
+import android.os.Bundle;
+
+public class TrackListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracklist);
+    }
+}
